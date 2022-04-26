e2e769660a8762f12f5792a65f7bc5872798c149
Huỳnh Ngọc Trung <mac@Macs-MacBook-Air.local>
2022-04-26 18:01:02 +0700

    final-project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './sass/index.scss';
+
+import Layout from './components/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Layout />
+    </Provider>
   </React.StrictMode>
 );
 
